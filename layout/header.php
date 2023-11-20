04a6ef0de6f4849d2d171777f3003f7b529678da
junaid-khan2 <jk9044465@gmail.com>
2023-11-20 20:27:23 +0500

    first commit

--- layout/header.php
+++ layout/header.php
@@ -2,6 +2,7 @@
     <img class="logo" src="./assets/logo.svg" />
     <div class="menu">
         <a href="./">Home</a>
+        <a href="./schedule.php">Schedule</a>
         <a href="./events.php">Events</a>
         <a href="./news.php">News</a>
     </div>
---
+++ schedule.php
@@ -0,0 +1,22 @@
+<?php
+
+
+include("./init.php");
+
+$page_title = "Schedule";
+?>
+
+
+<html>
+
+<?php include("layout/head.php"); ?>
+
+<body class="home-layout">
+    <?php include("layout/header.php"); ?>
+
+    <div class="page-content">
+      
+    </div>
+</body>
+
+</html>
